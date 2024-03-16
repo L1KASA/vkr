8a14e529bf304736bd87f28416103cd7de2eb00b
Anzhelika Takhmazova <43497908+L1KASA@users.noreply.github.com>
2024-03-17 00:16:33 +0400

    Чистка кода

--- main.py
+++ main.py
@@ -1,35 +1,27 @@
+
 from docx.shared import Pt, Cm
-from docx.enum.text import WD_LINE_SPACING
-from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.enum.text import WD_LINE_SPACING, WD_ALIGN_PARAGRAPH
 from docx import Document
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-from flet import Page,TextField
 import flet as ft
 from flet.matplotlib_chart import MatplotlibChart
 import os
-from docx.shared import Pt
-from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
-from docx.enum.text import WD_LINE_SPACING
 
 matplotlib.use("svg")
 temp_filename = "temp_plot.png"
-#buf = None
 
 def main(page: ft.Page):
-    #buf = None
     def window_event(e):
         print(e.data)
         if e.data == "close":
             print('page closed')
-            #buf.close() if buf is not None else page.window_destroy()
-            
+         
             if os.path.exists(temp_filename) is True:
                 print(12) 
                 os.remove(temp_filename) 
             page.window_destroy()
-            #page.window_close()
 
     page.window_prevent_close = True
     page.on_window_event = window_event
@@ -58,16 +50,9 @@ def main(page: ft.Page):
     )
 
     def handle_menu_item_click(e):
-        #print(f"{e.control.content.value}.on_click")
-        #page.show_snack_bar(ft.SnackBar(content=ft.Text(f"{e.control.content.value} was clicked!")))
-        #appbar_text_ref.current.value = e.control.content.value
-        #page.update()
-        #e.control.page.dialog = dlg
         page.dialog = dlg
         dlg.open = True
         page.update()
-        #e.control.page.update()
-        
 
     def handle_on_open(e):
         print(f"{e.control.content.value}.on_open")
@@ -82,7 +67,6 @@ def main(page: ft.Page):
         if os.path.exists(temp_filename) is True: 
             os.remove(temp_filename)
         page.window_destroy()
-
     
     menubar = ft.MenuBar(
         expand=True,
@@ -104,7 +88,6 @@ def main(page: ft.Page):
                         leading=ft.Icon(ft.icons.INFO),
                         style=ft.ButtonStyle(bgcolor={ft.MaterialState.HOVERED: ft.colors.GREEN_500}),
                         on_click=handle_menu_item_click
-                        #on_click=dlg
                     ),
                     ft.MenuItemButton(
                         content=ft.Text("Save"),
@@ -125,17 +108,11 @@ def main(page: ft.Page):
                         leading=ft.Icon(ft.icons.INFO),
                         style=ft.ButtonStyle(bgcolor={ft.MaterialState.HOVERED: ft.colors.GREEN_500}),
                         on_click=handle_menu_item_click
-                        #on_click=dlg
                     )
         ]
     )
 
-    def button_clicked(e):
-        t.value = f"Textboxes values are:  '{tb4.value}'"
-        page.update()
-
     def tb3_changes(field, pagee):
-        #print(str(tb4.on_change)[-1])
         c1.disabled = False
         c2.disabled = False
         c1.update()
@@ -151,18 +128,13 @@ def main(page: ft.Page):
             except ValueError:
                 field.value = last_valid_value
                 page.update()
-    
 
     t = ft.Text()
     tb3 = ft.TextField(label="With placeholder", hint_text="Пожалуйста, введите здесь данные",  on_change= lambda e: tb3_changes(tb3, page))
     tb4 = ft.TextField(label="With placeholder", hint_text="Please enter text here",  on_change= lambda e: tb3_changes(tb3, page))
-    
-    #b = ft.ElevatedButton(text="Submit", on_click=button_clicked)
-    #page.add(tb4)
 
     def c1_click(d):
         print(tb4.value)
-        #page.update()
         if str(tb3.value) == '' or str(tb4.value) == '':
             print('Пожалуйста, введите')
             return
@@ -171,10 +143,9 @@ def main(page: ft.Page):
 
         dt = 0.01
         t = np.arange(0, 30, dt)
-        nse1 = np.random.randn(len(t))  # white noise 1
-        nse2 = np.random.randn(len(t))  # white noise 2
+        nse1 = np.random.randn(len(t))
+        nse2 = np.random.randn(len(t))
 
-        # Two signals with a coherent part at 10Hz and a random part
         s1 = np.sin(float(tb4.value) * np.pi * float(tb3.value) * t) + nse1
         s2 = np.sin(float(tb4.value) * np.pi * float(tb4.value) * t) + nse2
        
@@ -189,58 +160,24 @@ def main(page: ft.Page):
         axs[1].set_ylabel("coherence")
         
         fig.tight_layout()
-
-        #plt.close()
-        #buf.value = io.BytesIO()
-        #fig.savefig(buf, format="png", bbox_inches="tight", pad_inches=0.1)
         fig.savefig(temp_filename, format="png", bbox_inches="tight", pad_inches=0.1)
-        
-        #print(buf.value)
-        #print(buf)
-        #buf.seek(0)
-        #print(buf)
-        
-        plt.close()
-        
-        #file_picker = ft.FilePicker()
-        #page.overlay.append(file_picker)
-        #file_picker.save_file()
-        #page.update()   
+        plt.close()  
         page.update()
-        #ax.legend(title="Fruit color")
-
-    # enable explicit updates
-    # and add chart to a page
-        #chart1 = MatplotlibChart(fig, isolated=True, expand=True)
-        
-        #chart1.clean()
-        #chart1 = MatplotlibChart(fig, isolated=True, expand=True)
-        
-
-        #sleep(5)
-
-        # update chart axis
-        #ax.legend(title="Colors")
         chart1.figure = fig
         chart1.update()
 
-       
     c1 = ft.Container(
         content=ft.ElevatedButton("Вычислить"),
-        #bgcolor=ft.colors.YELLOW,
         padding=5,
         on_click=c1_click,
         disabled=True       
     )
     c2 = ft.Container(
         content=ft.ElevatedButton("Elevated Button in Container"),
-        #bgcolor=ft.colors.YELLOW,
         padding=5,
         on_click=c1_click,
         disabled=True       
     )
-
-    
 
     def mysavefile(e:ft.FilePickerResultEvent):
         if not os.path.exists(temp_filename):
@@ -273,16 +210,13 @@ def main(page: ft.Page):
         document.add_picture(temp_filename)
         style = document.paragraphs[-1]
         style.alignment = 1  # 1 соответствует центру
-        #style.paragraph_format.first_line_indent = Cm(1.27)
-        #rr = document.add_paragraph(f"Первый набор данных: {float(tb4.value)}")
+
         document.add_paragraph("Рисунок 1 – Исходный график с данными")
         style = document.paragraphs[-1]
         style.alignment = 1  # 1 соответствует центру
         
         rr = document.add_paragraph(f"Первый набор данных: {float(tb4.value)}")
         rr.paragraph_format.first_line_indent = Cm(1.27)
-        #run = p.add_run('Графики и данные')
-
         
  # Сохраняем документ
         print(save_loc)
@@ -290,28 +224,16 @@ def main(page: ft.Page):
             save_loc = save_loc + '.docx'
         document.save(save_loc)
         
-        #messagebox.showinfo("Сохранение в docx", "Графики и данные успешно сохранены в документе Word.")
         page.show_snack_bar(ft.SnackBar(content=ft.Text("Графики и данные успешно сохранены в документе Word")))
-#
+
         page.update()
 
     saveme = ft.FilePicker(on_result=mysavefile)
     page.overlay.append(saveme)
-           
-    # Fixing random state for reproducibility
-    #page.add(ft.Row([menubar]), tb3, tb4, ft.Row(controls=[c1, c2]), c)
-    #page.add(
-    #    ft.Column([
-    #        ft.ElevatedButton("Save as file",
-    #                       on_click=lambda _: saveme.save_file())
-    #    ])
-    #)
 
     fig, ax = plt.subplots(2,1)
     chart1 = MatplotlibChart(fig, isolated=True, expand=True)
     plt.close()
     page.add(ft.Row([menubar]), tb3, tb4, ft.Row(controls=[c1, c2]), chart1, c)
-    #page.add(chart1)
     
 ft.app(target=main)
-#ft.app(target=main, view=ft.AppView.WEB_BROWSER)
